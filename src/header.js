0044c168d4bee5f66c3b4a1605238025ffbfe88c
Brayden Lynch <Brayden_@outlook.com>
2022-01-08 12:30:30 +1000

    Creation of header files and adding to app.js

---
+++ src/header.js
@@ -0,0 +1,13 @@
+// Header component for personal-website React App
+import React from 'react';
+import './header.css';
+
+function Header() {
+    return (
+        <header>
+            <p>I am a header</p>
+        </header>
+    )
+}
+
+export default Header
