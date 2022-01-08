da7d5bceab7c3229379d5d189c49e21598d1f98a
Brayden Lynch <Brayden_@outlook.com>
2022-01-08 18:40:02 +1000

    Header changed to Body. Style from Figma file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from './header.js';
+import Body from './body.js';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/body.js
@@ -0,0 +1,25 @@
+// Body element for personal-website app
+import './body.css';
+
+function Body() {
+    return (
+        <body>
+            <div className="introduction-module">
+                <div className="introduction-module-title">
+                    <h2>Hello there!</h2>
+                    <h6>I'm Brayden</h6>
+                </div>
+            </div>
+            <div className="information-module">
+                <div className="technical-skills">
+                    <p>Technical skills</p>
+                </div>
+                <div className="work-experiences">
+
+                </div>
+            </div>
+        </body>
+    )
+}
+
+export default Body;
