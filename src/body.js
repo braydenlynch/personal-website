09f48b33f86fa990b7d90d2f92d96ef58594d07b
Brayden Lynch <Brayden_@outlook.com>
2022-01-13 14:13:43 +1000

    Finished introduction module styling

--- src/body.js
+++ src/body.js
@@ -1,6 +1,8 @@
 // Body element for personal-website app
 import React from 'react';
 import { createUseStyles } from "react-jss";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IconContext } from 'react-icons/lib';
 
 function Body() {
     const classes = useStyles();
@@ -10,6 +12,19 @@ function Body() {
                 <div className={classes.introductionModuleTitle}>
                     <h2>Hello there!</h2>
                     <h6>I'm Brayden</h6>
+                    <p>I am driven to contribute to beautiful and powerful tech.</p>
+                    <div className={classes.introductionModuleIcons}>
+                        <a href="https://github.com/braydenlynch" target="_blank">
+                            <IconContext.Provider value = {{ className: classes.githubIcon}}>
+                                <FaGithub />
+                            </IconContext.Provider>
+                        </a>
+                        <a href="https://www.linkedin.com/in/braydenlynch/" target="_blank">
+                            <IconContext.Provider value = {{ className: classes.linkedinIcon}}>
+                                <FaLinkedin />
+                            </IconContext.Provider>
+                        </a>
+                    </div>
                 </div>
             </div>
             <div className={classes.informationModule}>
@@ -37,22 +52,40 @@ const useStyles = createUseStyles({
         float: "left",
     },
     introductionModuleTitle: {
-        marginTop: "25%",
+        marginTop: "15%",
         '& h2': {
-            paddingRight: "30px",
+            paddingRight: "15%",
             marginBottom: "0px",
             fontSize: "64px",
         },
         '& h6': {
-            paddingLeft: "20px",
+            paddingLeft: "15%",
             fontSize: "36px",
-            marginTop: "20px",
+            marginTop: "2.5%",
+            fontWeight: "lighter",
+        },
+        '& p': {
+            marginTop: "20%",
+            fontSize: "18px",
         },
     },
     informationModule: {
         width: "50vw",
         height: "50vh",
         float: "right",
+    },
+    introductionModuleIcons: {
+        marginTop: "20%",
+    },
+    githubIcon: {
+        color: "#e8eaea",
+        fontSize: "30px",
+        paddingRight: "0.5%",
+    },
+    linkedinIcon: {
+        color: "#00a0dc",
+        fontSize: "30px",
+        paddingLeft: "0.5%",
     },
 });
 
